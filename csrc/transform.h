d74b2c6d687f3628461b6c7200e68c7e1ce5f689
KhoiDOO <khoido8899@gmail.com>
2026-04-24 14:00:55 +0100

    feat: declare transform utilities

---
+++ csrc/transform.h
@@ -0,0 +1,33 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <stddef.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void generate_rotation_x_matrix(float angle, float* matrix);
+
+void generate_rotation_y_matrix(float angle, float* matrix);
+
+void generate_rotation_z_matrix(float angle, float* matrix);
+
+void generate_translation_matrix(float tx, float ty, float tz, float* matrix);
+
+void generate_scaling_matrix(float sx, float sy, float sz, float* matrix);
+
+void generate_fused_transform_matrix(float tx, float ty, float tz, float angle_x, float angle_y, float angle_z, float sx, float sy, float sz, float* matrix);
+
+void do_homogeneous_points(float* points, float* homogeneous_points, size_t num_points);
+
+void undo_homogeneous_points(float* points, float* homogeneous_points, size_t num_points);
+
+void transpose_matrix(const float* matrix, float* transposed_matrix, int rows, int cols);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TRANSFORM_H
