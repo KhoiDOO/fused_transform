2caf3d82889608ae280ed07c68eb6723a84dc563
KhoiDOO <khoido8899@gmail.com>
2026-04-24 14:00:55 +0100

    feat: declare cube surface sampler

---
+++ csrc/sample_p_cube.h
@@ -0,0 +1,16 @@
+#ifndef SAMPLE_P_CUBE_H
+#define SAMPLE_P_CUBE_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void sample_cube_surface(float *points, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
